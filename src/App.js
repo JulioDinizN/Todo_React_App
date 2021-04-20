9af27e6f75b1e30f75a1d2f40e37064ceb73eaad
Julio <juliocesardiniznogueira@gmail.com>
2021-04-20 15:08:18 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+
+import "./styles/global.css";
+
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    const getLocalTodos = () => {
+      if (localStorage.getItem("todos") === null) {
+        localStorage.setItem("todos", JSON.stringify([]));
+      } else {
+        let localTodo = JSON.parse(localStorage.getItem("todos"));
+        setTodos(localTodo);
+      }
+    };
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "uncompleted":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    filterHandler();
+  }, [todos, status]);
+
+  useEffect(() => {
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+    saveLocalTodos();
+  }, [todos, status]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Lista de Afazeres</h1>
+      </header>
+      <TodoForm
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,40 @@
+import nextId from "react-id-generator";
+
+const TodoForm = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { text: inputText, completed: false, id: nextId() }]);
+    setInputText("");
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">Todos</option>
+          <option value="completed">Completos</option>
+          <option value="uncompleted">Incompletos</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default TodoForm;
